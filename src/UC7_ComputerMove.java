b2f789c0aea3c1a1e881aeff8dba433dcccb194c
rs5301-hub <rs5301@srmist.edu.in>
2026-04-21 18:49:36 +0530

    Update UC7_ComputerMove.java

--- src/UC7_ComputerMove.java
+++ src/UC7_ComputerMove.java
@@ -1,10 +1,5 @@
 import java.util.Random;
 
-/**
- * TicTacToe
- * UC7 allows the computer to make a random valid move
- * by reusing slot conversion and validation logic.
- */
 public class TicTacToe {
 
     static char[][] board = {
@@ -14,6 +9,3 @@ public class TicTacToe {
     };
     static char computerSymbol = 'O';
 
-    /**
-     * Entry point of the program. Triggers the computer move.
-     */
