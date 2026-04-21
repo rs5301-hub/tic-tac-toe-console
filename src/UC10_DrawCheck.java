46c7d166fdd1677fa53fa73b204364f51fc99d3b
Rakshita Shekar <rs5301@srmist.edu.in>
2026-04-21 19:04:24 +0530

    UC10: Detect draw when no empty cells remain

---
+++ src/UC10_DrawCheck.java
@@ -0,0 +1,23 @@
+public class UC10_DrawCheck {
+
+    static char[][] board = {
+        {'X', 'O', 'X'},
+        {'X', 'O', 'O'},
+        {'O', 'X', 'X'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println("Is Draw? " + isDraw());
+    }
+
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
